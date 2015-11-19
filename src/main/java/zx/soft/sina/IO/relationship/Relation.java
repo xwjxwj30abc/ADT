ecc83fcfa4343a765a56236b7a26365f18861188
xuwenjuan <xwjfznxl@gmail.com>
2015-11-19 11:02:10 +0800

    微调微博转发接口获取

--- src/main/java/zx/soft/sina/IO/relationship/Relation.java
+++ src/main/java/zx/soft/sina/IO/relationship/Relation.java
@@ -18,7 +18,7 @@ public class Relation {
 	private static Logger logger = LoggerFactory.getLogger(Relation.class);
 
 	private static final String COOKIE = "SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFkT5yBjUZExKNJDuy2PW445JpX5KMt; UOR=www.chinaz.com,widget.weibo.com,login.sina.com.cn; SINAGLOBAL=4774358005242.229.1404870921222; ULV=1413773333874:51:10:1:2119691141365.4128.1413773333866:1413456152178; ALF=1445309399; wvr=5; SUS=SID-1854067572-1413773402-GZ-yfh2c-a128b79202962112a83158db7a51d952; SUE=es%3D72104da67ccfc14c5560bfc55f6bcff0%26ev%3Dv1%26es2%3D4bdb1977f777eb9b1c59cc2252adbb2b%26rs0%3DL8aHGdXR8ScyLLdLmYeM44%252F6kzvSJGdZGl%252FymLVU%252BO50A2r8tnvfvrT%252Ft7D0n00ibLTzE%252FOHkzftB%252BmEk57HvsMfXed%252F%252FXhko3%252BqjOLvk3jQC7yjbff4Nn5m%252FO2ygZN4Qn5gyVFxVDDLk2QXSxlHqlT1VryqxTEAvfwvcIdw8hM%253D%26rv%3D0; SUP=cv%3D1%26bt%3D1413773402%26et%3D1413859802%26d%3Dc909%26i%3Dd952%26us%3D1%26vf%3D0%26vt%3D0%26ac%3D0%26st%3D0%26uid%3D1854067572%26name%3D18256911213%2540sina.cn%26nick%3D182%252A%252A%252A%252A%252A213%2540sina.cn%26fmp%3D%26lcp%3D; SUB=_2AkMjGPdta8NlrAJZnfwVymzna49H-jyQzv2bAn7uJhIyGxgv7kdVqSUJJjOdUZXvnZWk3VI1y76p_h9iQA..; SSOLoginState=1413773402; _s_tentry=login.sina.com.cn; Apache=2119691141365.4128.1413773333866; JSESSIONID=494DA94A475EAB3D720565F4824030A3";
-	private static final int PAGE_COUNT = 50;
+	private static final int PAGE_COUNT = 200;
 
 	public static List<Weibo> getWeibos(String screenName, String source, SinaWeiboAPI api) {
 		List<Weibo> weibos = new ArrayList<>();
@@ -83,11 +83,14 @@ public class Relation {
 
 	public static void main(String[] args) {
 		SinaWeiboAPI api = new SinaWeiboAPI(new HttpClientDaoImpl());
-		List<Weibo> weibos = Relation.getWeibos("暂时胆小", "3845272542", api);
+		//List<Weibo> weibos = Relation.getWeibos("暂时胆小", "3845272542", api);
 		//System.out.println(weibos.size());
-		List<Comments> comments = Relation.getComments("3905364770682830", api, "3442868347");//3194139109
-		List<Comments> reposts = Relation.getReposts("3905364770682830", api, "3442868347");//3194139109 861 461
-		System.out.println(comments.size());
-		System.out.println(reposts.size());
+		//List<Comments> comments = Relation.getComments("3905364770682830", api, "3845272542");//3194139109
+		for (int i = 0; i < 30; i++) {
+			List<Comments> reposts = Relation.getReposts("3905364770682830", api, "3194139109");//3194139109 861 461
+			System.out.println(reposts.size());
+		}
+		//System.out.println(comments.size());
+
 	}
 }
