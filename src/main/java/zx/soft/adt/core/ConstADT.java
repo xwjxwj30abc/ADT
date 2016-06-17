9b9a4dbe3d85f9f38a851fee79648481d370d4ec
xuwenjuan <xwjfznxl@gmail.com>
2016-06-17 10:31:55 +0800

    移除tools_tmp文件

--- src/main/java/zx/soft/adt/core/ConstADT.java
+++ src/main/java/zx/soft/adt/core/ConstADT.java
@@ -11,9 +11,4 @@ public class ConstADT {
 	public static String TABLE_VPN_WAN_IPV4 = "adt.vpn_wlan_ip";//impala
 	public static String TABLE_HOT_PLUG_LOG = "adt.hot_plug_log";//impala
 
-	public static final String StringFields = "Net_ending_name,Keyword1,Keyword2,Keyword3,Net_ending_ipv6,Destination_ipv6,"
-			+ "Keyword21,Keyword22,Keyword23,Keyword24,Keyword25,Country_name,Rule_id,Destination_ipv6,Net_ending_ipv6,User_name,"
-			+ "Certificate_type,Certificate_code,Org_name,Country,Service_name,Address,Zip,Principal,Principal_tel,"
-			+ "Infor_man,Infor_man_tel,Infor_man_email,Ip,Net_monitor_department,Net_monitor_man,Net_monitor_man_tel."
-			+ "Remark,SessionID,UdpHost,UdpVer,Device_ipv6,Device_serial,Device_version,Device_note,ipv4";
 }
